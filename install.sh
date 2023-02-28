3087ff02a704379c3aa16ede0591a268d8f55ba8
Coddra <andrew.reeds.mpg@gmail.com>
2023-02-28 18:59:33 +0100

    Fixed some aspects of install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 DIR=$PWD
 
 # INSTALLATION
-sudo pacman -S --needed alacritty autotiling chromium clang discord doas dotnet-sdk emacs fish inotify-tools libreoffice light llvm obs-studio onboard pdfjs picom polybar python-adblock python-pip qutebrowser ranger touchegg ueberzug vim
+sudo pacman -S archlinux-keyring
+sudo pacman -Syu
+sudo pacman -S --needed alacritty autotiling chromium clang cmake discord doas dotnet-sdk emacs fish inotify-tools libreoffice light llvm obs-studio onboard pdfjs picom polybar python-adblock python-pip qutebrowser ranger touchegg ueberzug vim
 
 # Create doas config as I would like to use it from now on
 echo "permit persist $USER as root" | sudo tee /etc/doas.conf
@@ -19,13 +21,11 @@ mkdir ~/Code/Build
 # Write
 wget http://www.styluslabs.com/download/write-tgz -O ~/Downloads/write-tgz
 tar -xvzf ~/Downloads/write-tgz -C ~/Code/Build
-doas cp ~/Code/Build/Write/Write /bin/write
+doas ln -s ~/Code/Build/Write/Write /bin/write
 
 # ScreanRot (my fork on GuLinux's screenrotator)
 git clone https://github.com/coddra/screanRot ~/Code/Build/ScreanRot
-cd ~/Code/Build/ScreanRot
-./install.sh
-cd $DIR
+~/Code/Build/ScreanRot/install.sh
 
 # Using paru to get additional packages from aur
 rsync --mkpath paru/paru.conf ~/.config/paru/paru.conf
@@ -48,17 +48,16 @@ doas systemctl start touchegg
 # Remove deprecated config file from polybar
 rm ~/.config/polybar/config
 
-# Set rsync flags based on "safe" argument
-if [ "$1" = "safe" ]; then
-    FLAGS="-au"
-else
+if [ "$1" = "force" ]; then
     FLAGS="-a"
+else
+    FLAGS="-au"
 fi
 # Copy my configuration files
 rsync $FLAGS config/ ~/.config/
 rsync $FLAGS doom/ ~/.doom.d/
 
 # REMOVE UNNECCESSARY PACKAGES AND FILES
-doas pacman -R firedragon conky-manager conky garuda-zsh-config zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting zsh
+doas pacman -R conky-manager conky firedragon garuda-zsh-config rofi zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting zsh
 
 rm -rf ~/.zshrc ~/.bashrc_garuda ~/.firedragon
